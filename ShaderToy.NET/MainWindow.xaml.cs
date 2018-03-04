9642515ac1dd3687acb56fb98ab94308288c0d47
cvelez <christian.velez55@gmail.com>
2018-03-04 01:21:21 -0400

    Small code cleanup

--- ShaderToy.NET/MainWindow.xaml.cs
+++ ShaderToy.NET/MainWindow.xaml.cs
@@ -30,24 +30,22 @@ namespace ShaderToy.NET
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        OpenGL gl;
-        ShaderScene scn;
-        List<Shader> shaders = new List<Shader>();
+        private readonly ShaderScene shaderScene;
+        private readonly List<Shader> shaders = new List<Shader>();
+        private readonly AudioPlayback aud = new AudioPlayback();
+        private readonly Microphone mic;
+        private readonly AudioBitmap audioBitmap = new AudioBitmap();
 
-        AudioPlayback aud = new AudioPlayback();
-        Microphone mic;
-
-        AudioBitmap ab = new AudioBitmap();
-
-        BitmapImage Ch0Image;
-
+        private BitmapImage Ch0Image;
+        private OpenGL gl;
+        
         public MainWindow()
         {
             InitializeComponent();
 
             LoadWasapiDevicesCombo();
             MicCombo.SelectedIndex = 0;
-            mic = new Microphone((MMDevice)MicCombo.SelectedItem);
+            mic = new Microphone((MMDevice) MicCombo.SelectedItem);
             
             aud.FftCalculated += OnFftCalculated;
             mic.FftCalculated += OnFftCalculated;
@@ -64,53 +62,42 @@ namespace ShaderToy.NET
             shaders.Add(new Shader("Sea", "sea"));
             shaders.Add(new Shader("Mandelbrot", "mandelbrot"));
 
-            scn = new ShaderScene(shaders[0]);
+            shaderScene = new ShaderScene(shaders[0]);
             shaderSelector.ItemsSource = shaders;
             shaderSelector.SelectedIndex = 0;
 
-            ab.OnBitmapUpdated += (s, a) => scn.UpdateTextureBitmap(gl, 0, a.image);
+            audioBitmap.OnBitmapUpdated += (s, a) => shaderScene.UpdateTextureBitmap(gl, 0, a.image);
         }
 
        
 
         private void OpenGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
         {
-            //OpenGL gl = args.OpenGL;
-
-            //scn.UpdateTextureBitmap(gl, 0, ImageHelper.BitmapImage2Bitmap(Ch0Image));
-            //scn.UpdateTextureBitmap(gl, 1, ab.lastBitmap);
-            //scn.UpdateTextureBitmap(gl, 1, ImageHelper.BitmapImage2Bitmap(Ch0Image));
-
-            scn.Draw(gl,(float)OpenGLControl.ActualWidth, (float)OpenGLControl.ActualHeight);
-            //scn.Draw(gl, 300, 300);
+            shaderScene.Draw(gl, (float) OpenGLControl.ActualWidth, (float) OpenGLControl.ActualHeight);
         }
 
         private void OpenGLControl_OpenGLInitialized(object sender, OpenGLEventArgs args)
         {
             gl = args.OpenGL;
-            scn.Initialise(gl);
+            shaderScene.Initialise(gl);
 
             //init channel 0 (image)
-            Ch0Image = new BitmapImage(new Uri(@"C:/sta.jpg"));
-            CHO_ImageBox.Source = Ch0Image;
-            scn.UpdateTextureBitmap(gl, 1, ImageHelper.BitmapImage2Bitmap(Ch0Image));
-            //scn.UpdateTextureBitmap(gl, 1, ab.lastBitmap);
-
-            //texture1.Create(gl, ImageHelper.BitmapImage2Bitmap(Ch0Image));
-            //texture1.TextureName = "iChannel1";
-
-            //init channel 1 (audio)
-            //texture2.Create(gl);
-            
-            
+            UpdateChannel0(@"C:\sta.png");
         }
 
-        public void OnFftCalculated(object sender,FftEventArgs e)
+        private void UpdateChannel0(string uriOfImage)
+        {
+            Ch0Image = new BitmapImage(new Uri(uriOfImage));
+            CHO_ImageBox.Source = Ch0Image;
+            shaderScene.UpdateTextureBitmap(gl, 1, ImageHelper.BitmapImage2Bitmap(Ch0Image));
+        }
+        
+        private void OnFftCalculated(object sender, FftEventArgs e)
         {
             NAudio.Dsp.Complex[] result = e.Result;
-            this.Dispatcher.Invoke(new Action(() => {
+            Dispatcher.Invoke(new Action(() => {
                 SpecAnalyser.Update(result);
-                ab.Update(OpenGLControl.OpenGL, result);
+                audioBitmap.Update(OpenGLControl.OpenGL, result);
             }));
         }
 
@@ -145,20 +132,20 @@ namespace ShaderToy.NET
             }
         }
 
-        bool isFullScreen = false;
+        private bool isFullScreen = false;
         private void MakeFullScreen(object sender, RoutedEventArgs e)
         {
             if (!isFullScreen)
             {
-                this.ShowTitleBar = false;
-                this.WindowState = WindowState.Maximized;
+                ShowTitleBar = false;
+                WindowState = WindowState.Maximized;
                 MainGrid.ColumnDefinitions[1].Width = new GridLength(0);
                 isFullScreen = true;
             }
             else
             {
-                this.ShowTitleBar = true;
-                this.WindowState = WindowState.Normal;
+                ShowTitleBar = true;
+                WindowState = WindowState.Normal;
                 MainGrid.ColumnDefinitions[1].Width = new GridLength(250);
                 isFullScreen = false;
             }
@@ -167,7 +154,7 @@ namespace ShaderToy.NET
 
         private void Shader_SelectedChanged(object sender, SelectionChangedEventArgs e)
         {
-            scn.ActiveShader = shaders[shaderSelector.SelectedIndex];
+            shaderScene.ActiveShader = shaders[shaderSelector.SelectedIndex];
         }
 
         private void Start_Mic(object sender, RoutedEventArgs e)
@@ -184,7 +171,6 @@ namespace ShaderToy.NET
             MicStart_Button.IsEnabled = true;
         }
 
-        //public ObservableCollection<MMDevice> MicDevices;
         private void LoadWasapiDevicesCombo()
         {
             var deviceEnum = new MMDeviceEnumerator();
@@ -203,11 +189,8 @@ namespace ShaderToy.NET
             bool? result = ofd.ShowDialog();
             if (result.HasValue && result.Value)
             {
-                Ch0Image = new BitmapImage(new Uri(ofd.FileName));
-                CHO_ImageBox.Source = Ch0Image;
-                scn.UpdateTextureBitmap(gl, 1, ImageHelper.BitmapImage2Bitmap(Ch0Image));
+                UpdateChannel0(ofd.FileName);
             }
-            
         }
     }
 }
